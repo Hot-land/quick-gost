dffb95224d3fb7dddf49bef243f831b31a3f6a3b
V <68182002+vpngm@users.noreply.github.com>
2023-03-12 19:31:35 +0800

    Update gost.sh

--- gost.sh
+++ gost.sh
@@ -103,6 +103,7 @@ function Install_ct() {
   mkdir /etc/gost && wget --no-check-certificate https://raw.githubusercontent.com/Hot-land/quick-gost/master/config.json && mv config.json /etc/gost && chmod -R 777 /etc/gost
   
   /sbin/restorecon -v /usr/lib/systemd/system/gost.service
+  /sbin/restorecon -v /usr/bin/gost
   
   systemctl enable gost && systemctl restart gost
   echo "------------------------------"
